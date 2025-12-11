a3784d5146255ce128861475509d295c0bf76223
thesmartwon <thesmartwon@pm.me>
2025-12-11 14:15:15 -0500

    start on conllu rendering

--- pkgs/web/src/index.ts
+++ pkgs/web/src/index.ts
@@ -2,9 +2,18 @@ import type {
 	AutoUpdateOptions,
 	ComputePositionConfig,
 } from "@floating-ui/dom";
-import { autoUpdate, computePosition, offset, shift } from "@floating-ui/dom";
+import {
+	autoUpdate,
+	computePosition,
+	flip,
+	offset,
+	shift,
+} from "@floating-ui/dom";
+import { conllu } from "@nerd-bible/core";
+import * as z from "@nerd-bible/valio";
 
-console.log("hello worldd");
+console.log("Welcome to nerd.Bible");
+
 const root = document.body;
 
 function getId<T extends HTMLElement>(id: string): T {
@@ -62,5 +71,59 @@ root.addEventListener("keydown", (ev) => {
 			.forEach((e) => {
 				e.removeAttribute("open");
 			});
+	}
+});
+
+const main = document.querySelector("main")!;
+
+const worker = new Worker(new URL("./worker", import.meta.url), {
+	type: "module",
+});
+
+worker.postMessage({ sentences: "gen" });
+worker.addEventListener("message", (ev) => {
+	main.innerHTML = "";
+
+	const sentences: z.Output<typeof conllu.normal> = ev.data;
+	let p: HTMLParagraphElement | undefined;
+	for (const s of sentences) {
+		const newpar = s.headers["newpar"];
+		if (newpar != null) {
+			if (p) main.appendChild(p);
+			p = document.createElement("p");
+			if (newpar) p.className = newpar;
+		}
+
+		for (const w of s.words) {
+			const span = document.createElement("span");
+			span.textContent = w.form;
+			for (const a in w) {
+				if (!["form", "feats", "deps", "misc"].includes(a)) {
+					span.setAttribute(a, w[a]);
+				}
+			}
+			p?.appendChild(span);
+			if (w.misc["SpaceAfter"] !== "No") {
+				p?.appendChild(new Text(" "));
+			}
+		}
+	}
+	if (p) main.appendChild(p);
+});
+
+const hoverBox = document.getElementById("wordHover")!;
+main.addEventListener("pointerdown", (ev) => {
+	if (ev.target instanceof HTMLElement && ev.target.tagName === "SPAN") {
+		hoverBox.innerText = "hello";
+		computePosition(ev.target, hoverBox, {
+			placement: "bottom",
+			middleware: [offset(2), flip()],
+		}).then(({ x, y }) => {
+			Object.assign(hoverBox.style, {
+				left: `${x}px`,
+				top: `${y}px`,
+				display: "inline-block",
+			});
+		});
 	}
 });
