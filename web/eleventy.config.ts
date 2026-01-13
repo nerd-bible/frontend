f58674ccb915e6579189521d8ab4dadc69abd61d
thesmartwon <thesmartwon@pm.me>
2026-01-13 16:20:59 -0500

    better css + image bundling

--- web/eleventy.config.ts
+++ web/eleventy.config.ts
@@ -4,22 +4,17 @@ import postcss from "postcss";
 import pluginIcons from "./build/plugins/eleventy/iconify.ts";
 import postcssConfig from "./postcss.config.js";
 
-const fontPath = import.meta
-	.resolve("@nerd-bible/fonts/dist")
-	.replace("file://", "");
-
 async function postcssTransform(content: string) {
+	content = `@import "@nerd-bible/fonts/dist/faces.css";\n${content}`;
 	const result = await postcss(postcssConfig as any).process(content, {
 		from: this.page.inputPath,
+		to: "dist/bundle/index.css",
 	});
 	for (const w of result.warnings()) console.warn(w.toString());
 	return result.css;
 }
 
 export default function (eleventyConfig: UserConfig) {
-	eleventyConfig.addPassthroughCopy("img");
-	eleventyConfig.addPassthroughCopy({ [fontPath]: "." });
-
 	eleventyConfig.addPlugin(pluginWebc, {
 		components: ["./components/**/*.webc", "npm:@11ty/is-land/*.webc"],
 		bundlePluginOptions: {
--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,7 +1,31 @@
 import atImport from "postcss-import";
+import resolveUrl from "postcss-url";
 import atApply from "./build/plugins/postcss/at-apply.ts";
 import atFunction from "./build/plugins/postcss/at-function.ts";
 
 export default {
-	plugins: [atImport(), atApply(), atFunction()],
+	plugins: [
+		atImport(),
+		atApply(),
+		atFunction(),
+		resolveUrl([
+			{
+				filter: /\.woff2$/,
+				url: "copy",
+				basePath: import.meta
+					.resolve("@nerd-bible/fonts/dist")
+					.replace("file://", ""),
+				assetsPath: "../fonts",
+				useHash: true,
+				hashOptions: { append: true },
+			},
+			{
+				filter: /\.(jpe?g|svg|png|webp)$/,
+				url: "copy",
+				basePath: "img",
+				assetsPath: "../img",
+				useHash: false,
+			},
+		]),
+	],
 };
