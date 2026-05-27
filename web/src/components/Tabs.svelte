76293cadcf329460c416630a5fbef2b95f67fa35
thesmartwon <thesmartwon@pm.me>
2026-05-27 14:01:35 -0400

    tabs styles

--- web/src/components/Tabs.svelte
+++ web/src/components/Tabs.svelte
@@ -26,7 +26,42 @@ const uid = $props.id();
 </section>
 
 <style>
+section {
+	--border-size: 2px;
+	--border-color: var(--color-bg-200);
+	--border: var(--border-size) solid var(--border-color);
+	padding: --spacing(4);
+}
+div[role="tablist"] {
+	display: flex;
+	align-items: baseline;
+	gap: --spacing(2);
+	overflow: auto;
+	padding: 0 --spacing(2);
+	padding-top: --spacing(2);
+	margin-bottom: calc(-1 * var(--border-size));
+	position: relative;
+	z-index: 2;
+}
 div[role="tabpanel"] {
 	padding: --spacing(4);
+	border: var(--border);
+	border-radius: var(--radius-md);
+	position: relative;
+	z-index: 1;
+}
+button {
+	border-radius: var(--radius-md) var(--radius-md) 0 0;
+	border: var(--border-size) solid transparent;
+	border-bottom: none;
+	&[aria-selected="true"] {
+		background: var(--color-bg-50);
+		border-color: var(--border-color);
+	}
+
+	&[aria-selected="false"] {
+		padding-bottom: --spacing(1);
+		margin-bottom: --spacing(1);
+	}
 }
 </style>
